437488f09fd7758f01aac35c8f71726f4a41dd84
Jonek <81569423+Jimjomjam@users.noreply.github.com>
2021-07-03 20:18:09 +0200

    Add files via upload

---
+++ rng.js
@@ -0,0 +1,5 @@
+const generateBtn = document.querySelector(".generation");
+
+generateBtn.addEventListener("click", function() {
+alert('Thank you for checking if this works! Unfortunetally, it is not.');
+});
