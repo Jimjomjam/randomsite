97a0824e48871522f8b6ac92daf005c6c462f46c
Jonek <81569423+Jimjomjam@users.noreply.github.com>
2021-07-04 12:24:07 +0200

    Delete rng.js

--- rng.js
+++
@@ -1,5 +0,0 @@
-const generateBtn = document.querySelector(".generation");
-
-generateBtn.addEventListener("click", function() {
-alert('Thank you for checking if this works! Unfortunetally, it is not.');
-});
