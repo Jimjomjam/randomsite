e88c0ca515453a80892c0761d5f650c1792b1651
Jonek <81569423+Jimjomjam@users.noreply.github.com>
2021-07-06 19:10:44 +0200

    Delete rng.js

--- rng.js
+++
@@ -1,8 +0,0 @@
-const generateBtn = document.querySelector(".generation");
-
-generateBtn.addEventListener("click", function() {
-    let lowNum = document.querySelector("#lownumber").value;
-    let highNum = document.querySelector("#highnumber").value;
-    let generated = Math.floor(Math.random() * highNum  - lowNum);
-    document.getElementById("output").innerHTML = generated;
-});
